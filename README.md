3cf1509deb007574d21dff63e40704912abe5f77
hbouallegue <hilmi.bouallegue@geomatys.com>
2016-04-29 10:57:57 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Angular2 App Tour of Heroes
+================================================
+* First install the dependecies of the project
+
+      npm install
+
+ * For run the app :
+
+       npm start
---
+++ src/components/hero-details.component.ts
@@ -0,0 +1,35 @@
+// We begin by importing the Component and Input decorators from Angular
+
+import {Component, Input} from 'angular2/core';
+import {Hero} from '../shared/hero';
+
+//Define the component annotation (Decorator) for configure the component
+
+// We create metadata with the @Component decorator
+@Component({
+    selector : "<my-hero-detail>",
+    template: `
+          <div *ngIf="hero">
+           <h2>{{hero.name}} details!</h2>
+            <div><label>id: </label>{{hero.id}}</div>
+             <div>
+                <label>name: </label>
+                    <input [(ngModel)]="hero.name" placeholder="name"/>
+                </div>
+                </div>
+                        `
+
+})
+
+// Define the class component for define the manipulation
+// of the view and define the properties & the behaviors
+
+//we export the class to make it available to other components.
+
+export class HeroDetailComponent {
+    @Input()
+    hero: Hero;
+
+
+
+}
